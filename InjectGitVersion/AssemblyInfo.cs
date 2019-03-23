aae53f0eee3740b4bf8dd4c0b6fd1cc0c84e0228
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-03-23 20:56:35 +0100

    init

---
+++ InjectGitVersion/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectGitVersion
+{
+    public static class AssemblyInfo
+    {
+        public static T GetAssemblyAttribute<T>(Assembly assembly)
+            where T : Attribute
+        {
+            object[] attributes = assembly.GetCustomAttributes(typeof(T), true);
+            if ((attributes == null) || (attributes.Length == 0))
+            {
+                return null;
+            }
+
+            return (T)attributes[0];
+        }
+    }
+}
---
+++ InjectGitVersion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace InjectGitVersion
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var version = AssemblyInfo.GetAssemblyAttribute<AssemblyFileVersionAttribute>(assembly).Version;
+
+            Console.WriteLine("Version: " + version);
+            Console.ReadKey();
+        }
+    }
+}
